d23552ee861570b67767427b7e8c5c6ec9506d35
SHERV22 <73649619+SHERV22@users.noreply.github.com>
2021-05-01 15:06:14 +0530

    Add files via upload

---
+++ js/Hero.js
@@ -0,0 +1,29 @@
+class Hero{
+    constructor(x,y,r){
+        var options = {
+        density: 1,
+        frictionAir: 1
+        };
+            this.x=x;
+            this.y=y;
+            this.r=r;
+            this.image=loadImage("images/Superhero-01.png");
+            this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
+            World.add(world, this.body);
+            
+    }
+    display(){
+        var pos =this.body.position;
+        var angle =this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        /*ellipseMode(CENTER);
+        strokeWeight(4);
+        stroke("black");
+        fill("red");
+        ellipse(0, 0, this.r,this.r);*/
+        image(this.image,0,0,this.r,this.r)
+        pop();
+    }
+}
